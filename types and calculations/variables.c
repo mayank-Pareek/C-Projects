fadfbc6e31e9202c182110514420e841bdabc39f
mayank-Pareek <mpareek365@gmail.com>
2022-10-16 09:53:35 -0400

    create required varialbles Created suitable variables and stored some values in them for test purposes.

---
+++ types and calculations/variables.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int pid1 = 111, pid2 = 222, pid3 = 111;
+    float pricep1 = 111.49, pricep2 = 222.99, pricep3 = 334.49;
+    float avgprice = (pricep1 + pricep2 + pricep3)/3;
+    char taxp1 = 'Y', taxp2 = 'N', taxp3 = 'N';
+    const double testValue = 330.99;
+
+    return 0;
+}
